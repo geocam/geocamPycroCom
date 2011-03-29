5d24e3adf5587abefb2538f0ffc783b95b1e0ed8
trey.smith@nasa.gov <trey.smith@nasa.gov>
2011-03-29 09:20:06 -0700

    removed hooks to CMU IPC

--- IpcWrapper.py
+++
@@ -1,6 +0,0 @@
-
-from cmuIpcPackage import CmuIpc
-from PubSubWrapper import PubSubWrapper
-
-class IpcWrapper(CmuIpc, PubSubWrapper):
-    pollForMessages = True
--- exampleConfig.py
+++ exampleConfig.py
@@ -1,10 +1,6 @@
 PROTOCOL = 'tcp'
 
-if PROTOCOL == 'ipc':
-    SERVER_ENDPOINT = 'ipc:exampleServer'
-    NOTIFY_ENDPOINT = 'ipc:localhost:1381'
-    USE_SERVICE_DISCOVERY = True
-elif PROTOCOL == 'tcp':
+if PROTOCOL == 'tcp':
     SERVER_ENDPOINT = 'tcp:localhost:9085'
     NOTIFY_ENDPOINT = None
     USE_SERVICE_DISCOVERY = False
